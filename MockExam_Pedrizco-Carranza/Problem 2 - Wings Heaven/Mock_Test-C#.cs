0489a2a8085908725623bb49d91361157ca7afc6
Izzy Pedrizco-Carranz (RIT Student) <ip7431@rit.edu>
2020-09-16 10:57:03 -0400

    Add project files.

---
+++ MockExam_Pedrizco-Carranza/Problem 2 - Wings Heaven/Mock_Test-C#.cs
@@ -0,0 +1,72 @@
+﻿using System;
+
+namespace Problem_2___Wings_Heaven
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            const double TAX = 0.08;
+            const double DISCOUNT = 0.18;
+            double boneless = 7.99;
+            double traditionalWing = 9.99;
+            double bothWings = boneless + traditionalWing;
+
+            double[] typeOfSauce = new double[4];
+            typeOfSauce[0] = 0.29;
+            typeOfSauce[1] = 0.39;
+            typeOfSauce[2] = 0.99;
+            typeOfSauce[3] = 0.00;
+
+            int[] numberForList = new int[4];
+            numberForList[0] = 1;
+            numberForList[1] = 2;
+            numberForList[2] = 3;
+            numberForList[3] = 4;
+
+            int chooseSauces;
+            int chooseSides;
+
+            double waffleFries = 3.49;
+            double onionRings = 2.49;
+            double bothSides = waffleFries + onionRings;
+            double subtotal;
+            double total;
+
+            Console.Write($"Enter the kind of wings [b - boneless ({boneless:C}), t - traditional " +
+                $"({traditionalWing:C}), c - both ({bothWings:C}): ");
+            Console.ForegroundColor = ConsoleColor.Green;
+            string optionOfWings = Console.ReadLine();
+            Console.ResetColor();
+
+            Console.WriteLine($"{numberForList[0]}. Buffalo Sauce ({typeOfSauce[0]:C})" +
+                $"\n{numberForList[1]}. Mild Sauce ({typeOfSauce[1]:C})" +
+                $"\n{numberForList[2]}. BBQ Sauce ({typeOfSauce[2]:C})" +
+                $"\n{numberForList[3]}. Plain (no charge)");
+
+            Console.Write("\nSelect one of the preferred sauces: ");
+            Console.ForegroundColor = ConsoleColor.Green;
+            int.TryParse(Console.ReadLine(), out chooseSauces);
+            Console.ResetColor();
+
+            Console.WriteLine($"{numberForList[0]}. Waffle Fries ({waffleFries:C}" +
+                $"\n{numberForList[1]}. Onion RIngs ({onionRings:C})" +
+                $"\n{numberForList[2]}. Both ({bothSides:C})" +
+                $"\n{numberForList[3]}. None");
+            Console.Write("Select one of the sides: ");
+            Console.ForegroundColor = ConsoleColor.Green;
+            int.TryParse(Console.ReadLine(), out chooseSides);
+
+            if (optionOfWings == "boneless")
+            {
+                if (chooseSauces == numberForList[0])
+                {
+                    if (chooseSides == numberForList[0])
+                    {
+
+                    }
+                }
+            }
+        }
+    }
+}
---
+++ MockExam_Pedrizco-Carranza/Program.cs
@@ -0,0 +1,80 @@
+﻿using System;
+
+namespace MockExam_Pedrizco_Carranza
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            string[] listOfChoice = new string[3];
+            listOfChoice[0] = "1.";
+            listOfChoice[1] = "2.";
+            listOfChoice[2] = "3.";
+
+            int[] listOfChoiceChosen = new int[3];
+            listOfChoiceChosen[0] = 1;
+            listOfChoiceChosen[1] = 2;
+            listOfChoiceChosen[2] = 3;
+
+            const double PRICE_OF_PUZZLE_GAMES = 19.95;
+            int numberOfPuzzleGames;
+            double totalPriceOfPuzzleGames;
+            int chosenOption;
+            int amountOfGamePurchased;
+            int amountOfGameSold;
+            int currentAmountOfGame;
+
+            Console.WriteLine("*** Welcome to the Puzzle Hunters store! ***");
+            Console.Write("Enter the number of the puzzle games currently in the store: ");
+            Console.ForegroundColor = ConsoleColor.Green;
+            int.TryParse(Console.ReadLine(), out numberOfPuzzleGames);
+            Console.ResetColor();
+
+            totalPriceOfPuzzleGames = numberOfPuzzleGames * PRICE_OF_PUZZLE_GAMES;
+
+            Console.WriteLine($"{listOfChoice[0]} Purchase puzzle games from the customers" +
+                $"\n{listOfChoice[1]} Sell puzzle games to the customers" +
+                $"\n{listOfChoice[2]} Display the current value of the inventory");
+            Console.Write("Select one of the options: ");
+            Console.ForegroundColor = ConsoleColor.Green;
+            int.TryParse(Console.ReadLine(), out chosenOption);
+            Console.ResetColor();
+
+            //List of options that user chose
+            if (chosenOption == listOfChoiceChosen[0])
+            {
+                //If purchasing, amount of games will lose, but the owner will gain some currency values.
+                Console.Write("How many puzzle games would you like to purchase: ");
+                Console.ForegroundColor = ConsoleColor.Green;
+                int.TryParse(Console.ReadLine(), out amountOfGamePurchased);
+                Console.ResetColor();
+                currentAmountOfGame = numberOfPuzzleGames - amountOfGamePurchased;
+                totalPriceOfPuzzleGames =  totalPriceOfPuzzleGames + (amountOfGamePurchased * PRICE_OF_PUZZLE_GAMES);
+
+                Console.WriteLine("Puzzle Games Inventory Valuation\n");
+                Console.WriteLine($"Current number of puzzle games in the store - {currentAmountOfGame}" +
+                    $"\nCurrent value - {totalPriceOfPuzzleGames:C}");
+            }
+            else if (chosenOption == listOfChoiceChosen[1])
+            {
+                //It is the opposite of purchase...
+                Console.Write("How many puzzle games would you like to sell: ");
+                Console.ForegroundColor = ConsoleColor.Green;
+                int.TryParse(Console.ReadLine(), out amountOfGameSold);
+                Console.ResetColor();
+                currentAmountOfGame = numberOfPuzzleGames - amountOfGameSold;
+                totalPriceOfPuzzleGames = totalPriceOfPuzzleGames + (amountOfGameSold * PRICE_OF_PUZZLE_GAMES);
+
+                Console.WriteLine("Puzzle Games Inventory Valuation\n");
+                Console.WriteLine($"Current number of puzzle games in the store - {currentAmountOfGame}" +
+                    $"\nCurrent value - {totalPriceOfPuzzleGames:C}");
+            }
+            else if (chosenOption == listOfChoiceChosen[2])
+            {
+                Console.WriteLine("Puzzle Games Inventory Valuation\n");
+                Console.WriteLine($"Current number of puzzle games in the store - {numberOfPuzzleGames}" +
+                    $"\nCurrent value - {totalPriceOfPuzzleGames:C}");
+            }
+        }
+    }
+}
